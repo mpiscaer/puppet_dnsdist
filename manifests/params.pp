a2f836b5ed36643c6e1a383f5455bf27691a0f69
Tom Kaminski <tom.kaminski@futurenet.com>
2021-09-23 13:20:50 -0500

    debian support, allow distribution package, add api and control keys, add webserver acl

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,10 +5,14 @@ class dnsdist::params {
   $version          = '13'
   $webserver        = '0.0.0.0:80'
   $webserver_pass   = 'geheim'
+  $api_key          = undef
   $control_socket   = '127.0.0.1'
-  $listen_addresess = '0.0.0.0'
+  $control_socket_key  = undef
+  $listen_addresess = ['0.0.0.0']
   $cache_enabled    = false
   $cache_size       = 10000
   $metrics_enabled  = false
   $metrics_host     = '127.0.0.1'
+  $os               = downcase($facts['os']['name'])
+  $use_upstream_package_source = true
 }
